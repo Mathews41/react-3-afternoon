554130a809553484f901abaa0d77f56d587e9b8e
Mark Mathews <marklmathews4@gmail.com>
2019-08-11 14:14:17 -0600

    couldnt get the post to work

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 import './App.css';
 
 import Header from './Header/Header';
 import Compose from './Compose/Compose';
+import Post from './Post/Post';
 
 class App extends Component {
   constructor() {
@@ -16,21 +18,35 @@ class App extends Component {
     this.updatePost = this.updatePost.bind( this );
     this.deletePost = this.deletePost.bind( this );
     this.createPost = this.createPost.bind( this );
+    this.componentDidMount = this.componentDidMount.bind(this);
   }
   
-  componentDidMount() {
+  componentDidMount() { 
+    axios.get('https://practiceapi.devmountain.com/api/posts').then((response) => { 
+      this.setState({posts: response.data })
+  }).catch((error)=> console.log(error))
 
   }
 
-  updatePost() {
+  updatePost(id, text) {
+    axios.put(`https://practiceapi.devmountain.com/api/posts?id=${ id }`).then( results => {
+      this.setState({ posts: results.data})
+    })
   
   }
 
-  deletePost() {
+  deletePost(id) {
+    axios.delete(`https://practiceapi.devmountain.com/api/posts?id=${ id }`).then( results => {
+      this.setState({ posts: results.data})
+    });
+
 
   }
 
-  createPost() {
+  createPost(text) {
+    axios.post(`https://practiceapi.devmountain.com/api/posts?id=${ text }`).then( results => {
+      this.setState({ posts: results.data})
+    }).catch()
 
   }
 
@@ -42,8 +58,20 @@ class App extends Component {
         <Header />
 
         <section className="App__content">
+          <Compose createPostFn={ this.createPost } />
 
           <Compose />
+          {
+            posts.map( post => (
+              <Post key={ post.id } 
+              id={ post.id }
+                text={ post.text}
+                date={ post.date}
+                updatePostFn={ this.updatePost }
+                deletePostFn={ this.deletePost } />
+               
+            ))
+          }
           
         </section>
       </div>
